ef031bfe06c739a8a1f1d64d08be9e05b9c5923d
01Sriharsha <harshavasu463@gmail.com>
2024-03-17 16:45:14 +0530

    added linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'quotes-props': 'off',
+    'disable-quotes' : 'on'
+  }
+}
