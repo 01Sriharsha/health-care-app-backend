9bf2e303244ad5c0c55b68603207ff2215e0f8a5
01Sriharsha <harshavasu463@gmail.com>
2024-03-18 23:52:11 +0530

    refactored auth controller

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model.js";
-import { ApiError, ApiSuccess } from "../util/ApiResponse.js";
+import { Admin } from "../models/admin.model.js";
+import { ApiError } from "../util/ApiResponse.js";
 import { asyncHandler } from "../util/asyncHandler.js";
+import { validateObject } from "../util/validateObject.js";
 
 const COOKIE_NAME = "token";
 
@@ -11,94 +13,139 @@ const options = {
 };
 
 export const handleOAuthLogin = asyncHandler(async (req, res, next) => {
-  if (req.user.id) {
-    const user = await User.findById({ _id: req.user.id });
-    if (!user) {
-      return ApiError(res, 401, "User not found");
+  try {
+    if (req.user.id) {
+      const user = await User.findById({ _id: req.user.id });
+      if (!user) {
+        return ApiError(res, 401, "User not found");
+      }
+      const token = await user.generateAccessToken();
+      return res.cookie("token", token, { httpOnly: true, path: "/" }).json({
+        success: true,
+        message: "User logged successfully",
+        user,
+      });
+    } else {
+      return ApiError(res, 401, "Failed to login");
     }
-    const token = await user.generateAccessToken();
-    return res.cookie("token", token, { httpOnly: true, path: "/" }).json({
-      success: true,
-      message: "User logged successfully",
-      user,
-    });
-  } else {
-    return ApiError(res, 401, "Failed to login");
+  } catch (error) {
+    return ApiError(res, 500, error?.message);
   }
 });
 
 export const registerUser = asyncHandler(async (req, res) => {
-  const { fullname, email, phone, password, role, city, gender } = req.body;
+  try {
+    const { fullname, email, phone, password, role, city, gender } = req.body;
 
-  if (!fullname || !email || !phone || !password || !role || !city || !gender) {
-    return ApiError(res, 400, "All fields are required!");
+    const { error } = validateObject({
+      fullname,
+      email,
+      phone,
+      password,
+      role,
+      city,
+      gender,
+    });
+
+    if (error) {
+      return ApiError(res, 400, error);
+    }
+
+    //Check if email matches with admin
+    const isAdmin = await Admin.findOne({ email });
+
+    const existingUser = await User.findOne({ $or: [{ email }, { phone }] });
+
+    if (isAdmin || existingUser) {
+      return ApiError(res, 400, "Email already exists!");
+    }
+
+    const user = await User.create({
+      fullname,
+      password,
+      email,
+      phone,
+      city,
+      gender,
+      role,
+      emailVerfied: false,
+    }).catch((err) => ApiError(res, 400, err.message));
+
+    const savedUser = await User.findById(user._id).select("-password");
+
+    if (!savedUser) {
+      return ApiError(res, 500, "Failed to create user!");
+    }
+
+    const token = await savedUser.generateAccessToken();
+
+    return res.status(201).cookie(COOKIE_NAME, token, options).json({
+      user: savedUser,
+    });
+  } catch (error) {
+    return ApiError(res, 500, error?.message);
   }
-
-  const existingUser = await User.findOne({ $or: [{ email }, { phone }] });
-
-  if (existingUser) {
-    return ApiError(res, 400, "User already exists!");
-  }
-
-  const user = await User.create({
-    fullname,
-    password,
-    email,
-    phone,
-    city,
-    gender,
-    role,
-    emailVerfied: false,
-  }).catch((err) => ApiError(res, 400, err.message));
-
-  const savedUser = await User.findById(user._id).select("-password");
-
-  if (!savedUser) {
-    return ApiError(res, 500, "Failed to create user!");
-  }
-
-  const token = await savedUser.generateAccessToken();
-
-  return res.status(201).cookie(COOKIE_NAME, token, options).json({
-    user: savedUser,
-  });
 });
 
 export const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    return ApiError(res, 400, "Email and Password is required!");
+    const { error } = validateObject({ email, password });
+
+    if (error) {
+      return ApiError(res, 400, error);
+    }
+
+    //Check for user
+    let user = await User.findOne({ email });
+
+    //Check for admin
+    if (!user) {
+      user = await Admin.findOne({ email });
+    }
+
+    //Both not found throw error
+    if (!user) {
+      return ApiError(res, 401, "Email not found");
+    }
+
+    const isPasswordCorrect = await user.isPasswordCorrect(password);
+
+    if (!isPasswordCorrect) {
+      return ApiError(res, 401, "Invalid Password");
+    }
+
+    const token = await user.generateAccessToken();
+
+    let loggedInUser;
+    if (user.role === "ADMIN") {
+      loggedInUser = await Admin.findById(user._id).select("-password"); // remove the password from response
+    } else {
+      loggedInUser = await User.findById(user._id).select("-password");
+    }
+
+    return res
+      .status(200)
+      .cookie(COOKIE_NAME, token, options)
+      .json({
+        message: `Welcome back ${
+          user.role === "ADMIN" ? "ADMIN" : user.fullname?.toUpperCase()
+        }!`,
+        user: loggedInUser,
+      });
+  } catch (error) {
+    return ApiError(res, 500, error?.message);
   }
-
-  const user = await User.findOne({ email });
-
-  if (!user) {
-    return ApiError(res, 401, "Email not found");
-  }
-
-  const isPasswordCorrect = await user.isPasswordCorrect(password);
-
-  if (!isPasswordCorrect) {
-    return ApiError(res, 401, "Invalid Password");
-  }
-
-  const token = await user.generateAccessToken();
-
-  const loggedInUser = await User.findById(user._id).select("-password");
-
-  res
-    .status(200)
-    .cookie(COOKIE_NAME, token, options)
-    .json({
-      message: `Welcome back ${user.fullname?.toUpperCase()}!`,
-      user: loggedInUser,
-    });
 });
 
 export const logoutUser = asyncHandler(async (req, res) => {
-  req.logout();
-  return res.status(200).clearCookie(COOKIE_NAME, options).json({
-    message: "Logged Out Successfully",
-  });
+  try {
+    req.logout();
+    return res.status(200).clearCookie(COOKIE_NAME, options).json({
+      message: "Logged Out Successfully",
+    });
+  } catch (error) {
+    return ApiError(res, 500, error?.message);
+  }
 });
